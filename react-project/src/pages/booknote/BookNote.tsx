7386e050d57e08bbeccc5822cdf675f3809b23d2
Subin <80036437+hansb1023@users.noreply.github.com>
2022-10-05 04:02:55 +0900

    [Update] 레이아웃 변경 중

--- react-project/src/pages/booknote/BookNote.tsx
+++ react-project/src/pages/booknote/BookNote.tsx
@@ -3,15 +3,19 @@ import { Col, Row } from "react-bootstrap"
 import { Container } from "react-bootstrap"
 import { ReadingList } from "../../components/ReadingList"
 import { Writing } from "../../components/Writing"
+import * as React from 'react';
+import Layout from '../../components/Layout';
+import Sidebar from '../../components/Sidebar';
+import Main from '../../components/Main';
 import readingItems from "../../data/readinglist.json"
 
-export function BookNote() {
+const BookNote: React.FC = () => {
     return (
-        <>
+        <Layout>
+            <Sidebar>
                 <Row span={5}>
                     <h1>독서 기록</h1>
                     <p>읽고 있는 책 중에서 기록하고자 하는 책을 선택하세요.</p>
-                
                     <ul>
                         {readingItems.map(item => (
                             <li key={item.id}>
@@ -20,10 +24,14 @@ export function BookNote() {
                             ))}
                     </ul>
                 </Row>
-
+            </Sidebar>
+            <Main>           
                 <Row span={20}>
                             <p>기록하는 부분</p>
                 </Row>
-        </>
+            </Main>     
+        </Layout>
     )
 }
+
+export default BookNote;

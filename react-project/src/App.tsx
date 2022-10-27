3b1b2dc5c97154ed277c3ba77848c97087659c4b
Subin <80036437+hansb1023@users.noreply.github.com>
2022-10-27 19:21:08 +0900

    [Update] 도서상세페이지 폴더 및 파일 생성

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { BookNote } from "./pages/booknote/BookNote"
 
+
 function App() {
   return (
     <Container className="mb-4">

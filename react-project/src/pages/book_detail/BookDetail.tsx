75a625c43ab8d34e112fdaf0d4fb0d65e7fde22e
Subin <80036437+hansb1023@users.noreply.github.com>
2022-10-31 20:45:38 +0900

    [Update] 도서상세페이지 제작 중

--- react-project/src/pages/book_detail/BookDetail.tsx
+++ react-project/src/pages/book_detail/BookDetail.tsx
@@ -1,3 +1,14 @@
 export function  BookDetail() {
-    return <h1>BookDetail</h1>
+    return (
+        <>
+            <div className="justify-content-flex-start">
+                <img src="/imgs/book1.jpg"></img>
+                <h1>책 제목 적기</h1>
+                <h2>카테고리 적기</h2>
+            </div>
+            <div className="Middle">
+                
+            </div>
+        </>
+    )
 }

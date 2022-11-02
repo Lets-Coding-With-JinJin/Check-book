be8cda7daebbebf9efb892343f1fc67eba1b1518
Subin <80036437+hansb1023@users.noreply.github.com>
2022-11-02 21:48:59 +0900

    [Modify] 새로운 브랜치 생성을 위한 정리

--- react-project/src/components/bookdetail.tsx
+++
@@ -1,4 +0,0 @@
-export function bookdetail(){
-
-    return <h1>bookdetail</h1>
-}
--- server/config/database.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-module.exports = {
-    host     : 'localhost',
-    user     : '< MySQL username >',
-    password : '< MySQL password >',
-    database : 'my_db'
-  };

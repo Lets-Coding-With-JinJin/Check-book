49ba18362c89d47afcf57ff4e9404541450a7b72
siyeooon <psy001225@ajou.ac.kr>
2022-11-06 16:16:47 +0900

    main-page

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Home.css';
+import { FiChevronRight } from "react-icons/fi";
+import Product from './product';
 
 function Home() {
     return (
@@ -8,8 +10,30 @@ function Home() {
                 <img className='home-event1' src="./mainevent1.png" alt="mainevent"/>
             </div>
 
-            <div className='rec'>박시연 님이 좋아하실만한 책을 소개해 드려요</div>
+        <div className='rec'>
+            <div className='rec-rec'>박시연 님이 좋아하실만한 책을 소개해 드려요</div>
             <div className='rec-age'>최근에 고등학생 연령대에 가장 많이 조회된 책이에요.</div>
+            <div className='rec-ctgy'>카테고리별 추천도서</div>
+            <div className='rec-review'>실시간 서평</div>
+            <button class="next">
+                <FiChevronRight className='icon-next' />
+            </button>
+            </div>
+
+        <div className='home_row'>
+            <Product />
+        </div>
+        <div className='home_row'>
+            <Product />
+        </div>
+        <div className='home_row'>
+            <Product />
+        </div>
+        <div className='home_row'>
+            <Product />
+        </div>
+
+
         </div>
     )
 }
---
+++ src/product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './product.css';
+
+function Product() {
+    return (
+        <div className='product'>
+            <h1>책 이미지 넣어야 됩니다 css적용 전</h1>
+        </div>
+    )
+}
+
+export default Product;

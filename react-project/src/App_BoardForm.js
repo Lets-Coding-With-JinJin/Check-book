8d8c1be94884b07e4444aa527f51147911d9fc89
Subin <80036437+hansb1023@users.noreply.github.com>
2022-11-17 16:38:33 +0900

    feature: 기록 하는 부분 구현(아직 책과 연결은 안됨)

--- react-project/src/App_BoardForm.js
+++ react-project/src/App_BoardForm.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
 import { firebase_board_save } from './App_reducer'
+
+import './App_BoardForm.css'
+
 
 class BoardForm extends Component {
     state = {};
     initialSelectedBoard = {
-        brdno: "",
-        brdtitle: "",
-        brdwriter: "",
+        booktitle: "",
+        booksummary: "",
+        booknote: "",
         brddate: ""
     };
 
@@ -27,11 +34,29 @@ class BoardForm extends Component {
     
     render() {
         return (
-            <div>
-                <input placeholder="title" name="brdtitle" value={this.state.brdtitle} onChange={this.handleChange} />
-                <input placeholder="name" name="brdwriter" value={this.state.brdwriter} onChange={this.handleChange} />
-                <button onClick={this.handleSave}>Save</button>
-            </div>
+            <>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '836px', top: '64px' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <div className="write column">
+                        <div className="booktitle"><TextField label="도서 제목을 입력하세요" placeholder="Placeholder"
+                            multiline variant="standard" rows={1} name="booktitle" value={this.state.booktitle} onChange={this.handleChange} />
+                        </div>
+                        <div className="booksummary"><TextField label="책에서 영감을 받은 부분 혹은 독서 기록을 한 문장으로 요약해주세요." placeholder="Placeholder"
+                            multiline variant="standard" rows={1} name="booksummary" value={this.state.booksummary} onChange={this.handleChange} />
+                        </div>
+                        <div className="booknote"><TextField label="독서 기록 내용을 작성해주세요" placeholder="Placeholder"
+                            multiline variant="standard" rows={20} name="booknote" value={this.state.booknote} onChange={this.handleChange} />
+                        </div>
+                        <div><Button onClick={this.handleSave}>독서기록 작성 완료</Button></div>
+                    </div>
+                </Box>
+            </>
         );
     }
 }

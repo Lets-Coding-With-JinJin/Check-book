7c114b5aa04df5c6cb479c14b2d6dafd378d9ed7
Subin <80036437+hansb1023@users.noreply.github.com>
2022-11-17 21:43:48 +0900

    feature: firebase 작업 중 (독서기록 페이지)

--- react-project/src/Firestore.js
+++ react-project/src/Firestore.js
@@ -1,8 +1,19 @@
-
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/analytics'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 var config = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATA_BASEURL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
+
 firebase.initializeApp(config);
 var firestore = firebase.firestore();
 

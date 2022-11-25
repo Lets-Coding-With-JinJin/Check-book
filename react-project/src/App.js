5b00d04b81be9065d4f4c17f652fcebbd9298235
uomf <kj632147@gmail.com>
2022-11-25 16:02:02 +0900

    [Modify] 디테일 페이지 css 최적화

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,15 +4,15 @@ import BoardForm from './App_BoardForm';
 import BoardList from './App_BoardList';
 import BookDetail from './App_BookDetail';
 import Header from './Header';
+import './App.css'
 
 class App extends Component {
     
     render() {
         return (
             <div>
-                
                 <Header/>
-                <BookDetail/>
+                <BookDetail />
             </div>
         );
     }

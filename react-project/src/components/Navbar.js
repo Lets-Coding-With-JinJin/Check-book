f4dd102ed6c6d30be9b9b66de317427a8d84a1e8
Subin <80036437+hansb1023@users.noreply.github.com>
2022-12-02 18:54:04 +0900

    feature: 네비게이션바 추가

---
+++ react-project/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+          <div className='sum'>
+              <div className='logo'>
+                <h1>CheckChack</h1>
+              </div>
+
+              <div className='nav'>
+                <div className="header-search">
+                    <input className="header-search-input" type="text" placeholder="도서명, ISBN, 카테고리" />
+                </div>
+                <div class='ul'>
+                      <div className='header-option'>
+                          <Link className='headeroptionone' to='/'>로그인</Link>
+                      </div>
+                      <div className='header-option'>
+                          <Link className='headeroptionone' to='/'>회원가입</Link>
+                      </div>
+                </div>
+             </div>
+          </div>    
+    )
+  }
+  export default Navbar;

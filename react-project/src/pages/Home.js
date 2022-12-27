7a3705ce79e7baf7a33bd9bb7d4f5bffd5cd34c7
imssonice <sl030lz@ajou.ac.kr>
2022-12-28 08:07:07 +0900

    책 추가

--- react-project/src/pages/Home.js
+++ react-project/src/pages/Home.js
@@ -13,7 +13,17 @@ function Home() {
             <Slider />
 
         <div className='rec'>
-            <div className='rec-rec'>박시연 님이 좋아하실만한 책을 소개해 드려요.</div>
+            <div className='rec-rec'>박시연 님이 좋아하실만한 책을 소개해 드려요.
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 grid">
+                            <img src="http://image.yes24.com/goods/111731427/XL" alt='책1'></img>
+                            <h5>브로콜리 평원의 혈투</h5>
+                            <p>듀나</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className='rec-age'>최근에 고등학생 연령대에 가장 많이 조회된 책이에요.</div>
             <div className='rec-ctgy'>카테고리별 추천도서</div>
             <button class="next">

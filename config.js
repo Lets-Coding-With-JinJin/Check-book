b820b04febb54826d40553b246db4727989caedc
imssonice <sl030lz@ajou.ac.kr>
2022-12-28 04:04:56 +0900

    파이어베이스 연결

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyA2b2Ijo1oPSZg00nr9x1XK2sliyx_t71I",
+    authDomain: "checkchack-cba2a.firebaseapp.com",
+    projectId: "checkchack-cba2a",
+    storageBucket: "checkchack-cba2a.appspot.com",
+    messagingSenderId: "1005964855023",
+    appId: "1:1005964855023:web:31b93093a3cb45dd5a7d1a",
+    measurementId: "G-E56E46EQMF"
+  }
